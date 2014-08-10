c58848b44155241d06c416c9d47ea2b85b3b7753
danbender <mail@danbender.net>
2014-08-10 13:32:28 +0200

    clean up

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -43,7 +43,6 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
     end
